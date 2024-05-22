86b56e01dc25060cd37b982fc1ea7bc8277bf038
trandanhtue <trandanhtue267@gmail.com>
2024-05-22 10:01:15 +0700

    TDT362_cau2

---
+++ BaiKiemTra/src/TDT362_Cau1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+import javax.swing.Spring;
+
+public class TDT362_Cau1 {
+   
+    public static void main(String[] args) {
+        string ten;
+        int mssv , date ;
+        
+    }
+}
+    
---
+++ BaiKiemTra/src/TDT362_Cau2.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TDT362_Cau2 {
+  
+   
+   
+    public static void main(String[] args) {
+        string ten;
+        int mssv , date ; 
+        Scanner sc = new Scanner(System.in);
+        public void nhapthongtin();
+         System.out.println("nhap ten : ");
+         ten = sc.nextLine();
+         System.out.println("nhap mssv: ");
+         mssv = sc.nextInt();
+         System.out.println("nhap ngay sinh: ");
+         date = sc.nextInt();
+         public void inthongtin();
+         System.out.println("ten la: " + ten);
+         System.out.println("mssv la: " +mssv );
+         System.out.println("ngay sinh la: "+date);
+
+         
+    }
+
+    
+}
+public static void main(String[] args) {
+    final TDT362_Person p = new TDT362_Person();
+    Scanner sc = new Scanner(System.in);
+    p.nhapthongtin;
+    p.inthongtin;
+    ArrayList<String> a = new ArrayList<>();
+    System.out.println("them thong tin: ");
+    int n =sc.nextLine();
+    a.get(n);
+    System.out.println("xoa thong tin: ");
+    a.remove(n);
+
+}
